43dd781ed1710723a70bb1ed829f11bb44fe3a0a
TheRealCain532 <brandoneprintiss@gmail.com>
2021-01-17 10:32:08 -0600

    Add project files.

---
+++ Functions/PS3API/Extra/GameShark.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MultiLib;
+using System.Windows.Forms;
+using System.Collections;
+namespace MultiLib
+{
+    public class GameShark
+    {
+        private Extension Extension
+        {
+            get { return new Extension(SelectAPI.ControlConsole); }
+        }
+        private CCAPI CCAPI
+        {
+            get { return new CCAPI(); }
+        }
+
+        uint BtnMonitor;
+        enum CodeType
+        {
+            ConstantWrite,
+            EqualTo,
+            DifferentTo,
+            LessThan,
+            GreaterThan,
+            IncrementValue,
+            DecrementValue,
+            UniversalActivator,
+            UniversalDeActivator,
+            AllCodeButton,
+            SerialRepeater,
+            ActivateAll,
+            ActivateAll_Delay,
+            CopyBytes,
+        }
+        private int[] SplitInt(int input)
+        {
+            int a = input;
+            a = Math.Abs(a);
+            int length = a.ToString().Length;
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = a % 10;
+                a /= 10;
+            }
+            Array.Reverse(array);
+            return array;
+        }
+        byte[] SplitToByte(int[] input)
+        {
+            byte[] array = new byte[input.Length];
+            for (int i = 0; i < array.Length; i++)
+                array[i] = Convert.ToByte(input[i]);
+            return array;
+        }
+        int ConcatInt(int x, int y)
+        {
+            return (int)(x * Math.Pow(10, y.ToString().Length)) + y;
+        }
+        int ConcatIntString(int x, int y)
+        {
+            string _x = x.ToString("X"), _y = y.ToString("X");
+            return (int)(int.Parse(_x) * Math.Pow(10, y.ToString().Length)) + int.Parse(_y);
+        }
+        private void Execute(CodeType T, uint[] address, byte[][] bytes)
+        {
+            Console.WriteLine("multiline is called");
+            byte[] compare = new byte[10];
+            int[] cmp = new int[10];
+            switch (T)
+            {
+                case CodeType.EqualTo: ///E0 D0
+                    compare = Extension.ReadBytes(address[0], 2);
+                    if (ByteCompare(compare, bytes[0]))
+                        Execute(T, address[1], bytes[1]);
+                    break;
+                case CodeType.DifferentTo: ///E1 D1
+                    compare = Extension.ReadBytes(address[0], 2);
+                    if (!ByteCompare(compare, bytes[0]))
+                        Execute(T, address, bytes);
+                    break;
+                case CodeType.LessThan: ///E2 D2
+                    compare = Extension.ReadBytes(address[0], 2);
+                    cmp[0] = bytes[0][0] + bytes[0][1];
+                    cmp[1] = compare[0] + compare[1];
+                    if (cmp[1] < cmp[0])
+                        Execute(T, address[1], bytes[1]);
+                    break;
+                case CodeType.GreaterThan: ///E3 D3
+                    compare = Extension.ReadBytes(address[0], 2);
+                    cmp[0] = bytes[0][0] + bytes[0][1];
+                    cmp[1] = compare[0] + compare[1];
+                    if (cmp[1] > cmp[0])
+                        Execute(T, address[1], bytes[1]);
+                    break;
+                case CodeType.UniversalActivator: ///D4
+                    compare = Extension.ReadBytes(address[0], 2);
+                    if (ByteCompare(compare, bytes[0]))
+                        Execute(T, address[1], bytes[1]);
+                    break;
+                case CodeType.SerialRepeater: ///50
+                    //length is 8, parsable is 6 50 00 XX YY 00 ZZ ==> XX = length YY = jump ZZ = To Add (as increment)
+                    //TTTTTTTT VVVV ==> TTTTTTTT = start address VVVV = value written at each address + ZZ
+                    address[0] = address[0] - 0x2cb3f0;
+                    int[] magic = SplitInt(int.Parse(address[0].ToString("X")));
+                    uint TTTTTTTT = address[1];
+                    int
+                        XX = ConcatIntString(magic[0], magic[1]),
+                        YY = ConcatInt(magic[2], magic[3]),
+                        ZZ = bytes[0][0] + bytes[0][1];
+                    byte[] VVVV = bytes[1];
+
+                    Console.WriteLine(string.Format("XX = {0} YY = {1} ZZ = {2} start = {3:X} writing {4}", XX, YY, ZZ, TTTTTTTT, BitConverter.ToString(VVVV)));
+                    for (int i = 0; i < XX; i++)
+                    {
+                        Extension.WriteBytes(TTTTTTTT, VVVV);
+                        if (ZZ > 0)
+                            VVVV = BitConverter.GetBytes(VVVV[0] + ZZ);
+                        TTTTTTTT = TTTTTTTT + (uint)YY;
+                    }
+                    break;
+                case CodeType.AllCodeButton: ///D5
+                    compare = Extension.ReadBytes(BtnMonitor, 2);
+                    if (ByteCompare(compare, bytes[0]))
+                        Execute(T, address[1], bytes[1]);
+                    break;
+                default: break;
+            }
+        }
+
+        private void Execute(CodeType T, uint address, byte[] bytes)
+        {
+            Console.WriteLine("single line is called");
+
+            //Array.Reverse(bytes);
+            byte[] compare = new byte[10];
+            int[] cmp = new int[10];
+            switch (T)
+            {
+                case CodeType.ConstantWrite: ///80 30
+                    Extension.WriteBytes(address, bytes);
+                    break;
+                case CodeType.UniversalDeActivator: ///D6
+                    //workin' on it
+                    break;
+                case CodeType.IncrementValue: ///20 
+                    compare = Extension.ReadBytes(address, 2);
+                    cmp[0] = bytes[0] + bytes[1]; // To Add
+                    cmp[1] = compare[0] + compare[1]; // Add to
+                    Extension.WriteBytes(address, BitConverter.GetBytes(cmp[0] + cmp[1]));
+                    break;
+                case CodeType.DecrementValue: ///21 
+                    compare = Extension.ReadBytes(address, 2);
+                    cmp[0] = bytes[0] + bytes[1]; // To Add
+                    cmp[1] = compare[0] + compare[1]; // Add to
+                    Extension.WriteBytes(address, BitConverter.GetBytes(cmp[0] - cmp[1]));
+                    break;
+                default :break;
+            }
+        }
+
+        public void GSWrite(string input)
+        {
+            switch (input.Substring(0, Math.Min(2, input.Length)))
+            {
+                case "80":
+                    Execute(CodeType.ConstantWrite, GSC(input), GSB(input));
+                    break;
+                case "30":
+                    Execute(CodeType.ConstantWrite, GSC(input), GSB(input));
+                    break;
+                case "E0":
+                    Execute(CodeType.ConstantWrite, GSC(input), GSB(input));
+                    break;
+                case "10":
+                    Execute(CodeType.IncrementValue, GSC(input), GSB(input));
+                    break;
+                case "20":
+                    Execute(CodeType.IncrementValue, GSC(input), GSB(input));
+                    break;
+                case "11":
+                    Execute(CodeType.DecrementValue, GSC(input), GSB(input));
+                    break;
+                case "21":
+                    Execute(CodeType.DecrementValue, GSC(input), GSB(input));
+                    break;
+            }
+
+        }
+        public void GSWrite(string[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i].Substring(0, Math.Min(2, input[i].Length)))
+                {
+                    case "E0":
+                        Execute(CodeType.EqualTo, GSC(input), GSB(input));
+                        break;
+                    case "D0":
+                        Execute(CodeType.EqualTo, GSC(input), GSB(input));
+                        break;
+                    case "E3":
+                        Execute(CodeType.GreaterThan, GSC(input), GSB(input));
+                        break;
+                    case "D3":
+                        Execute(CodeType.GreaterThan, GSC(input), GSB(input));
+                        break;
+                    case "E2":
+                        Execute(CodeType.LessThan, GSC(input), GSB(input));
+                        break;
+                    case "D2":
+                        Execute(CodeType.LessThan, GSC(input), GSB(input));
+                        break;
+                    case "50":
+                        Execute(CodeType.SerialRepeater, GSC(input), GSB(input));
+                        break;
+                    case "D5":
+                        Execute(CodeType.AllCodeButton, GSC(input), GSB(input));
+                        break;
+                }
+                GSWrite(input[i]);
+            }
+        }
+        private bool Is_Connected;
+        public Boolean Connect()
+        {
+            uint[] PID;
+            Is_Connected = CCAPI.ConnectTarget();
+            CCAPI.GetProcessList(out PID);
+            if (Is_Connected)
+                Is_Connected = CCAPI.AttachProcess(PID[0]) >= 0;
+            return Is_Connected;
+        }
+
+        public uint GSC(string GSCode)
+        {
+            return (Convert.ToUInt32(GSCode.Split(' ')[0].Remove(0, 2), 0x10) + 0x2cb3f0);
+        }
+        public uint[] GSC(string[] GSCode)
+        {
+            uint[] array = new uint[GSCode.Length];
+            for (int i = 0; i < GSCode.Length; i++)
+                array[i] = Convert.ToUInt32(GSCode[i].Split(' ')[0].Remove(0, 2), 0x10) + 0x2cb3f0;
+            return array;
+        }
+        public byte[] GSB(string input)
+
+        {
+            byte[] array = STB(input.Split(' ')[1]);
+            Array.Reverse(array);
+            return array;
+        }
+        public byte[][] GSB(string[] input)
+
+        {
+            byte[][] array = new byte[input.Length][];
+            for (int i = 0; i < input.Length; i++)
+            {
+                array[i] = STB(input[i].Split(' ')[1]);
+                Array.Reverse(array[i]);
+            }
+            return array;
+        }
+
+        private byte[] STB(string hex)
+        {
+            if ((hex.Length % 2) > 0)
+            {
+                hex = "0" + hex;
+            }
+            int length = hex.Length;
+            byte[] buffer = new byte[((length / 2) - 1) + 1];
+            for (int i = 0; i < length; i += 2)
+            {
+                buffer[i / 2] = Convert.ToByte(hex.Substring(i, 2), 0x10);
+            }
+            return buffer;
+        }
+
+        static bool ByteCompare(byte[] a1, byte[] a2)
+        {
+            return StructuralComparisons.StructuralEqualityComparer.Equals(a1, a2);
+        }
+    }
+}
---
+++ Functions/XboxAPI/XboxLib.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+using System.Threading;
+using System.Runtime.InteropServices;
+using XDevkit;
+using System.IO;
+using MultiLib.Properties;
+namespace MultiLib
+{
+    public class Xbdm
+    {
+        public enum RebootType
+        {
+            Cold,
+            Title
+        }
+
+        private readonly XboxMemoryStream _xboxMemoryStream;
+        private uint _xboxConnectionCode;
+        private XboxConsole _xboxConsole;
+        private IXboxDebugTarget _xboxDebugTarget;
+        private IXboxManager _xboxManager;
+
+        /// <summary>
+        ///     Create a new instance of the XBDM Communicator
+        /// </summary>
+        /// <param name="deviceIdent">The Name of IP of the XBox Console running xbdm.</param>
+        /// <param name="openConnection">Open a connection to the XBox Console</param>
+        public Xbdm()
+        {
+            ConnectTarget();
+        }
+
+        // Public Modifiers
+        public string DeviceIdent { get; private set; }
+        public string XboxType { get; private set; }
+        public bool IsConnected { get; private set; }
+        public XboxMemoryStream MemoryStream
+        {
+            get { return _xboxMemoryStream; }
+            //set { _xboxMemoryStream = value; }
+        }
+
+        // Public Functions
+        /// <summary>
+        ///     Update the Xbox Device Ident (XDK Name or IP)
+        /// </summary>
+        /// <param name="deviceIdent">The new XBox XDK Name or IP</param>
+        public void UpdateDeviceIdent(string deviceIdent)
+        {
+            if (DeviceIdent != deviceIdent)
+                Disconnect();
+            DeviceIdent = deviceIdent;
+        }
+
+        /// <summary>
+        ///     Open a connection to the XBox Console
+        /// </summary>
+        /// <returns>true if the connection was successful.</returns>
+        public bool ConnectTarget()
+        {
+            if (!IsConnected)
+            {
+                try
+                {
+                    _xboxManager = new XboxManager();
+                    _xboxConsole = _xboxManager.OpenConsole(_xboxManager.DefaultConsole);
+                    _xboxDebugTarget = _xboxConsole.DebugTarget;
+                    _xboxConnectionCode = _xboxConsole.OpenConnection(null);
+                }
+                catch
+                {
+                    _xboxManager = null;
+                    _xboxConsole = null;
+                    _xboxDebugTarget = null;
+                    return false;
+                }
+
+                try
+                {
+                    XboxType = _xboxConsole.ConsoleType.ToString();
+                }
+                catch
+                {
+                    XboxType = "Unable to get.";
+                }
+
+                IsConnected = true;
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///     Close the connection to the XBox Console
+        /// </summary>
+        public void Disconnect()
+        {
+            if (!IsConnected) return;
+
+            if (_xboxConsole != null)
+                _xboxConsole.CloseConnection(_xboxConnectionCode);
+
+            _xboxManager = null;
+            _xboxDebugTarget = null;
+            _xboxConsole = null;
+            IsConnected = false;
+        }
+
+        /// <summary>
+        ///     Send a string-based command, such as "bye", "reboot", "go", "stop"
+        /// </summary>
+        /// <param name="command">The command to send.</param>
+        /// <returns>The responce from the console, or null if sending the command failed.</returns>
+        public string SendStringCommand(string command)
+        {
+            if (!ConnectTarget())
+                return null;
+
+            string response;
+            _xboxConsole.SendTextCommand(_xboxConnectionCode, command, out response);
+            //if (!(response.Contains("202") | response.Contains("203")))
+            return response;
+            /*else
+                throw new Exception("String command wasn't accepted by the Xbox 360 Console. It might not be valid or the Xbox is just being annoying. The response was:\n" + response);*/
+        }
+
+        /// <summary>
+        ///     Freeze the XBox Console
+        /// </summary>
+        public void Freeze()
+        {
+            if (!ConnectTarget())
+                return;
+
+            SendStringCommand("stop");
+        }
+
+        /// <summary>
+        ///     UnFreeze the XBox Console
+        /// </summary>
+        public void Unfreeze()
+        {
+            if (!ConnectTarget())
+                return;
+
+            SendStringCommand("go");
+        }
+
+        /// <summary>
+        ///     Reboot the XBox Console
+        /// </summary>
+        /// <param name="rebootType">The type of Reboot to do (Cold or Title)</param>
+        public void Reboot(RebootType rebootType)
+        {
+            if (!ConnectTarget())
+                return;
+
+            switch (rebootType)
+            {
+                case RebootType.Cold:
+                    SendStringCommand("reboot");
+                    break;
+                case RebootType.Title:
+                    SendStringCommand("reboot");
+                    break;
+            }
+        }
+
+        /// <summary>
+        ///     Shutdown the XBox Console
+        /// </summary>
+        public void Shutdown()
+        {
+            if (!ConnectTarget())
+                return;
+
+            // Tell console to go bye-bye
+            SendStringCommand("bye");
+
+            Disconnect();
+        }
+
+        /// <summary>
+        ///     Save a screenshot from the XBox Console
+        /// </summary>
+        /// <param name="savePath">The location to save the image to.</param>
+        /// <param name="freezeDuring">Do you want to freeze while the screenshot is being taken.</param>
+        public bool GetScreenshot(string savePath, bool freezeDuring = false)
+        {
+            if (!ConnectTarget())
+                return false;
+
+            // Stop the Console
+            if (freezeDuring)
+                Freeze();
+
+            // Screensnap that console
+            _xboxConsole.ScreenShot(savePath);
+
+            // Start the Console
+            if (freezeDuring)
+                Unfreeze();
+
+            return true;
+        }
+        public Extension Extension
+        {
+            get { return new Extension(SelectAPI.XboxNeighborhood); }
+        }
+
+        public int GetMemory(uint offset, byte[] buffer)
+        {
+            uint bytesRead = 0;
+            _xboxDebugTarget.GetMemory(offset, (uint)buffer.Length, buffer, out bytesRead);
+            _xboxDebugTarget.InvalidateMemoryCache(true, offset, (uint)buffer.Length);
+            return (int)bytesRead;
+        }
+        public int GetMemory(ulong offset, byte[] buffer)
+        {
+            uint bytesRead = 0;
+            _xboxDebugTarget.GetMemory((uint)offset, (uint)buffer.Length, buffer, out bytesRead);
+            _xboxDebugTarget.InvalidateMemoryCache(true, (uint)offset, (uint)buffer.Length);
+            return (int)bytesRead;
+        }
+        public byte[] GetBytes(uint offset, uint length)
+        {
+            byte[] buffer = new byte[length];
+            GetMemory(offset, buffer);
+            return buffer;
+        }
+        public void SetMemory(uint Offset, byte[] buffer)
+        {
+            uint g = 0;
+            _xboxDebugTarget.SetMemory(Offset, (uint)buffer.Length, buffer, out g);
+        }
+        public void SetMemory(ulong Offset, byte[] buffer)
+        {
+            uint g = 0;
+            _xboxDebugTarget.SetMemory((uint)Offset, (uint)buffer.Length, buffer, out g);
+        }
+        public class XboxMemoryStream : Stream
+        {
+            // Private Modifiers
+            private readonly Xbdm _xbdm;
+
+            public XboxMemoryStream(Xbdm xbdm)
+            {
+                _xbdm = xbdm;
+                Position = 0;
+            }
+
+
+            // IO Functions
+            public override bool CanRead
+            {
+                get { return true; }
+            }
+
+            public override bool CanSeek
+            {
+                get { return true; }
+            }
+
+            public override bool CanWrite
+            {
+                get { return true; }
+            }
+
+            public override long Length
+            {
+                get { return 0x100000000; }
+            }
+
+            public override sealed long Position { get; set; }
+
+            public override void Flush()
+            {
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                if (!_xbdm.ConnectTarget())
+                    return 0;
+
+                bool alreadyStopped = true;
+                if (count > 20)
+                    _xbdm._xboxDebugTarget.Stop(out alreadyStopped);
+
+                uint bytesRead;
+                if (offset == 0)
+                {
+                    _xbdm._xboxDebugTarget.GetMemory((uint)Position, (uint)count, buffer, out bytesRead);
+                }
+                else
+                {
+                    // Offset isn't 0, so read into a temp buffer and then copy it into the output
+                    var tempBuffer = new byte[count];
+                    _xbdm._xboxDebugTarget.GetMemory((uint)Position, (uint)count, tempBuffer, out bytesRead);
+                    Buffer.BlockCopy(tempBuffer, 0, buffer, offset, count);
+                }
+                Position += bytesRead;
+
+                if (!alreadyStopped)
+                    _xbdm._xboxDebugTarget.Go(out alreadyStopped);
+
+                return (int)bytesRead;
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                switch (origin)
+                {
+                    case SeekOrigin.Begin:
+                        Position = offset;
+                        break;
+
+                    case SeekOrigin.Current:
+                        Position += offset;
+                        break;
+
+                    case SeekOrigin.End:
+                        Position = 0x100000000 - offset;
+                        break;
+                }
+                return Position;
+            }
+
+            public override void SetLength(long value)
+            {
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                if (!_xbdm.ConnectTarget())
+                    return;
+
+                bool alreadyStopped = true;
+                if (count > 20)
+                    _xbdm._xboxDebugTarget.Stop(out alreadyStopped);
+
+                byte[] pokeArray = buffer;
+                if (offset != 0)
+                {
+                    // Offset isn't 0, so copy into a second buffer before poking
+                    pokeArray = new byte[count];
+                    Buffer.BlockCopy(buffer, offset, pokeArray, 0, count);
+                }
+
+                uint bytesWritten;
+                _xbdm._xboxDebugTarget.SetMemory((uint)Position, (uint)count, pokeArray, out bytesWritten);
+                Position += bytesWritten;
+
+                if (!alreadyStopped)
+                    _xbdm._xboxDebugTarget.Go(out alreadyStopped);
+            }
+        }
+
+    }
+}
